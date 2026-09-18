dbc647242fed57e3c509adf3486519a77abc3941
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-09-19 01:36:41 +0530

    chore: list all branches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    app()
+}
+
+fn app() -> anyhow::Result<()> {
+    let cwd = std::env::current_dir()?;
+    let repo = git2::Repository::open(cwd)?;
+    let branches = repo.branches(None)?;
+
+    for branch in branches.flatten() {
+        println!("{:?}", branch.0.name()?);
+    }
+
+    Ok(())
 }
