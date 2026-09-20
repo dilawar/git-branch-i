0985e0ac430e95a768ed7d401067079cfa5e1c9f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-09-20 13:36:06 +0530

    feat: wiring is done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use anyhow::Context;
+use git2::Repository;
 use inquire::Select;
 use strum::IntoEnumIterator;
 
 struct AppState {
-    repo: git2::Repository,
-    branches: Vec<String>,
+    repo: Repository,
 }
 
 #[derive(Default, strum::EnumString, strum::EnumIter, strum::Display)]
@@ -14,15 +15,48 @@ enum BranchAction {
     Delete,
 }
 
+struct GitBranch<'a>(pub git2::Branch<'a>);
+
+impl<'a> GitBranch<'a> {
+    fn name(&self) -> Option<&str> {
+        self.0.name().ok().flatten()
+    }
+}
+
+impl std::fmt::Display for GitBranch<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}",
+            self.0
+                .name()
+                .map_err(|_e| std::fmt::Error)?
+                .unwrap_or_default()
+        )
+    }
+}
+
 impl BranchAction {
-    fn act_on(self, branch: &str) -> anyhow::Result<()> {
+    fn act_on(&self, branch: GitBranch<'_>, repo: &Repository) -> anyhow::Result<()> {
         // act on given BranchAction
+        let branch_name = branch.name().context("empty name")?;
         match self {
             Self::Checkout => {
+                let (object, reference) = repo.revparse_ext(branch_name)?;
+                anyhow::ensure!(
+                    reference.is_some(),
+                    "Can't checkout tree without a valid reference"
+                );
+                let refname = reference.expect("must be set");
+                anyhow::ensure!(refname.is_branch(), "reference is not a branch");
                 println!("checking out {branch}");
+                repo.checkout_tree(&object, None)?;
+                repo.set_head(refname.name().expect("must have a valid name"))?;
             }
             Self::Delete => {
                 println!("Deleting given branch {branch}");
+                let mut branch = branch.0;
+                branch.delete()?;
             }
         }
 
@@ -33,26 +67,17 @@ impl BranchAction {
 impl AppState {
     fn new() -> Self {
         Self {
-            repo: git2::Repository::open(".").expect("could not open repo"),
-            branches: vec![],
+            repo: Repository::open(".").expect("could not open repo"),
         }
     }
 
-    fn load_branches(&mut self) -> anyhow::Result<()> {
-        self.branches.clear();
-
-        let branches = self.repo.branches(None)?;
-        for b in branches.flatten() {
-            if let Some(name) = b.0.name()? {
-                self.branches.push(name.into());
-            }
-        }
-
-        Ok(())
-    }
-
-    fn branches(&self) -> impl Iterator<Item = &str> {
-        self.branches.iter().map(|x| x.as_str())
+    fn branches(&self) -> anyhow::Result<Vec<GitBranch<'_>>> {
+        Ok(self
+            .repo
+            .branches(None)?
+            .flatten()
+            .map(|x| GitBranch(x.0))
+            .collect())
     }
 }
 
@@ -63,17 +88,15 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_app(app_state: &mut AppState) -> anyhow::Result<()> {
-    app_state.load_branches()?;
-
-    let options = app_state.branches().collect();
+fn run_app(app_state: &AppState) -> anyhow::Result<()> {
+    let options = app_state.branches()?;
     let selected_branch = Select::new("Select a branch?", options).prompt()?;
 
     // select action on selected branch.
     let allowed_actions = BranchAction::iter().collect();
     let selected_action = Select::new("select action on this branch", allowed_actions).prompt()?;
 
-    selected_action.act_on(selected_branch)?;
+    selected_action.act_on(selected_branch, &app_state.repo)?;
 
     Ok(())
 }
